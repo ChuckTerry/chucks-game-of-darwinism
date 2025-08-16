1c4075f9aa7cd0a935db0f05766be33a0d9d7ccf
Chuck Terry <Chuck.Terry@Outlook.com>
2025-08-16 07:29:20 -0400

    Restructure for clarity

---
+++ javascript/constants/colors.js
@@ -0,0 +1,9 @@
+import { STATE } from './state.js';
+
+export const COLORS = {
+    [STATE.EMPTY]: '#000000',
+    [STATE.SPECIES_A]: '#e74c3c',
+    [STATE.SPECIES_B]: '#3498db',
+    [STATE.DISEASED]: '#2ecc71',
+    [STATE.CONTESTED]: '#f1c40f'
+};
---
+++ javascript/constants/state.js
@@ -0,0 +1,7 @@
+export const STATE = {
+    EMPTY: 0,
+    SPECIES_A: 1,
+    SPECIES_B: 2,
+    DISEASED: 3,
+    CONTESTED: 4
+};
